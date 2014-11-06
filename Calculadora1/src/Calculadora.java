d252ba4b6c5672443e26f6fa279abe8a4ff967bf
zjian_000 <zjian_000@zhuweijiang>
2014-11-06 19:07:35 +0100

    Missatge calculadora científica

--- Calculadora1/src/Calculadora.java
+++ Calculadora1/src/Calculadora.java
@@ -2,7 +2,7 @@
 public class Calculadora {
 
 	public static void main(String[] args) {
-		System.out.println("Sóc una calculadora simple");
+		System.out.println("Sóc una calculadora científica");
 	}
 
 }
